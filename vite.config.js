5397b465fa2fa61a21dd85c4b25d0d20b39568ca
muhammad rifa'i <rifairafai77@gmail.com>
2026-05-05 21:31:09 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: './', // 🔥 ini penting
-  plugins: [react()],
+  base: "/portofolio/",
+  plugins: [react(), tailwindcss()],
 })
